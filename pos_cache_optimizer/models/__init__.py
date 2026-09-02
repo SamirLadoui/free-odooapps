51b1eae34a5f4281494996ed791a99a68307405f
Samir Ladoui <whitexhunter23@gmail.com>
2026-09-02 10:55:19 +0100

    Drop the old pos_cache_optimizer
    It shipped no tests, existed only for this release, advertised redis, memcached
    and offline support that appeared nowhere in it, and carried a price on an LGPL
    licence. It is replaced, under the same name, by a rewrite in the paid
    repository that is tested on every release from 14.0 to 19.0.

--- pos_cache_optimizer/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import pos_config
-from . import pos_cache_config
-from . import pos_cache_analytics
-from . import pos_session
-from . import product_template
-from . import res_config_settings
